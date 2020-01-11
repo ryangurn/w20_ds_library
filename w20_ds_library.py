e85ca64a5a87d3d79ad3a68cda598bb5423a7ca4
Ryan Gurnick <ryangurnick@gmail.com>
2020-01-11 01:30:56 -0800

    Update w20_ds_library.py

--- w20_ds_library.py
+++ w20_ds_library.py
@@ -3,8 +3,8 @@ import string
 from typing import TypeVar, Callable
 dframe = TypeVar('pd.core.frame.DataFrame')
 
-def compile_vector(target_row, columns):
-  nt = target_row.drop(columns)
+def compile_vector(row, columns):
+  nt = row.drop(columns)
   return nt.to_list()
 
 def euclidean_distance(vect1:list ,vect2:list) -> float:
@@ -17,42 +17,45 @@ def euclidean_distance(vect1:list ,vect2:list) -> float:
     dist += (vect1[i] - vect2[i])**2
 
   return np.sqrt(dist)
-  #rest of your code below
 
-def ordered_distances(target_vector:list, crowd_table:dframe, dfunc:Callable) -> list:
+def ordered_distances(target_vector:list, crowd_table:dframe, answer_column:str, dfunc:Callable) -> list:
   assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
   assert callable(dfunc), f'dfunc not a function but instead a {type(dfunc)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
 
-  #your code goes here  
   distance_list = []
 
   for i in range(0, len(crowd_table)):
     row = crowd_table.iloc[i]
-    vect = compile_vector(row)
+    vect = compile_vector(row, [answer_column])
 
     distance_list.append((i, dfunc(target_vector, vect)))
 
   distance_list = sorted(distance_list, key = lambda x: x[1])
   return distance_list
-
-def knn(target_vector:list, crowd_table:dframe, k:int, dfunc:Callable) -> int:
+def knn(target_vector:list, crowd_table:dframe, answer_column:str, k:int, dfunc:Callable) -> int:
   assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
-  assert crowd_table['Survived'].isin([0,1]).all(), f"answer_column must be binary"
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+  assert crowd_table[answer_column].isin([0,1]).all(), f"answer_column must be binary"
   assert callable(dfunc), f'dfunc not a function but instead a {type(dfunc)}'
 
-  sort = ordered_distances(target_vector, crowd_table, dfunc)
+  sort = ordered_distances(target_vector, crowd_table, answer_column, dfunc)
   #your code goes here
   top_k = [i for i,d in sort[:k]]
   opinions = [crowd_table.loc[i, 'Survived'] for i in top_k]
   winner = 1 if opinions.count(1) > opinions.count(0) else 0
   return winner
 
-def knn_tester(test_table, crowd_table, k, dfunc:Callable) -> dict:
+def knn_tester(test_table, crowd_table, answer_column, k, dfunc:Callable) -> dict:
   assert isinstance(test_table, pd.core.frame.DataFrame), f'test_table not a dataframe but instead a {type(test_table)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
-  assert crowd_table['Survived'].isin([0,1]).all(), f"answer_column must be binary"
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+  assert crowd_table[answer_column].isin([0,1]).all(), f"answer_column must be binary"
   assert callable(dfunc), f'dfunc not a function but instead a {type(dfunc)}'
   
   overall_sum = 0
@@ -61,8 +64,8 @@ def knn_tester(test_table, crowd_table, k, dfunc:Callable) -> dict:
   false_positive = 0
   false_negative = 0
   for target_vector in range(0, len(test_table)):
-    vect = compile_vector(test_table.iloc[target_vector])
-    predicted = knn(vect, crowd_table, k, dfunc)
+    vect = compile_vector(test_table.iloc[target_vector], [answer_column])
+    predicted = knn(vect, crowd_table, answer_column, k, dfunc)
     actual = test_table.iloc[target_vector]['Survived']
     if predicted == actual == 1:
       true_positive += 1
